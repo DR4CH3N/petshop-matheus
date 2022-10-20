023e19ae575d4ac722ba1fab7210fe2d4a0fcd8c
Matheus Anthony <thehellc4t@gmail.com>
2022-10-20 09:34:26 -0300

    formatação na pagina produtos

--- src/pages/Produtos/Produtos.jsx
+++ src/pages/Produtos/Produtos.jsx
@@ -4,6 +4,36 @@ const Produtos = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Conheça os nossos produtos</h2>
+
+      <div className={estilos.produtos}>
+        <article>
+          <h3>Produto 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quod
+            repellat aspernatur ducimus, tempore, tempora reprehenderit officia
+            est laboriosam, similique vero optio assumenda minima perspiciatis
+            eos. Magni quos ducimus doloribus.
+          </p>
+        </article>
+        <article>
+          <h3>Produto 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quod
+            repellat aspernatur ducimus, tempore, tempora reprehenderit officia
+            est laboriosam, similique vero optio assumenda minima perspiciatis
+            eos. Magni quos ducimus doloribus.
+          </p>
+        </article>
+        <article>
+          <h3>Produto 3</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quod
+            repellat aspernatur ducimus, tempore, tempora reprehenderit officia
+            est laboriosam, similique vero optio assumenda minima perspiciatis
+            eos. Magni quos ducimus doloribus.
+          </p>
+        </article>
+      </div>
     </section>
   );
 };
