249f1c0855460274856e2b4cdc494413e036f119
Matheus Anthony <thehellc4t@gmail.com>
2022-10-20 09:25:48 -0300

    Criação basica das paginas

---
+++ src/pages/Produtos/Produtos.jsx
@@ -0,0 +1,11 @@
+import estilos from "./Produtos.module.css";
+
+const Produtos = () => {
+  return (
+    <section>
+      <h2 className={estilos.titulo_secao}>Conheça os nossos produtos</h2>
+    </section>
+  );
+};
+
+export default Produtos;
