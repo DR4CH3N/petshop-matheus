c030b72607acc49066b17483557b700b9bdfdbc5
Matheus Anthony <thehellc4t@gmail.com>
2022-10-27 09:12:00 -0300

    ajustes nas rota de categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Categoria from "./pages/Categoria/Categoria";
 import Contato from "./pages/Contato/Contato";
 import Home from "./pages/Home/Home";
 import Pagina404 from "./pages/Pagina404/Pagina404";
@@ -28,9 +29,16 @@ const App = () => {
               <Post />
             </Route>
 
+            {/* estamos usando o :nome em vez de :id com o propositso de ter uma URL amigavel (slug) */}
+
+            <Route exact path="/categoria/:nome">
+              <Categoria> </Categoria>
+            </Route>
+
             <Route path="/produtos">
               <Produtos />
             </Route>
+
             <Route path="/sobre">
               <Sobre />
             </Route>
---
+++ src/pages/Categoria/Categoria.jsx
@@ -0,0 +1,12 @@
+import estilos from "./Categoria.module.css";
+
+/* objetivo deste componente é exibiro ListaPosts filtrado pela categoria escolhida no menu ListaCetegori */
+const Categoria = () => {
+  return (
+    <section>
+      <h2 className={estilos.titulo_secao}>Pet noticias</h2>
+    </section>
+  );
+};
+
+export default Categoria;
